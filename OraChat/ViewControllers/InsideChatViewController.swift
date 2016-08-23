2c7ddc8f22615777c6cc4fc3c61d28ca57d3d05a
Haskel Ash <haskelash@gmail.com>
2016-08-23 10:41:12 -0700

    add basic table view for messages

---
+++ OraChat/ViewControllers/InsideChatViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InsideChatViewController.swift
+//  OraChat
+//
+//  Created by Haskel Ash on 8/23/16.
+//  Copyright © 2016 Haskel Ash. All rights reserved.
+//
+
+import UIKit
+
+class InsideChatViewController: UIViewController, UITableViewDataSource {
+
+    let identifier = "MessageCell"
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath)
+    }
+}
---
+++ OraChat/Views/MessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  MessageCell.swift
+//  OraChat
+//
+//  Created by Haskel Ash on 8/23/16.
+//  Copyright © 2016 Haskel Ash. All rights reserved.
+//
+
+import Cocoa
+
+class MessageCell: UITableViewCell {
+
+}
