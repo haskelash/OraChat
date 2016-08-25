816aab2fcfbe71f6b56a7488eeee2e7ad096551a
Haskel Ash <haskelash@gmail.com>
2016-08-25 10:16:22 -0700

    add callback for selecting contact

--- OraChat/ViewControllers/NewChatViewController.swift
+++ OraChat/ViewControllers/NewChatViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class NewChatViewController: UIViewController, UITableViewDataSource {
+class NewChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+
+    @IBOutlet private var chatNameField: UITextField!
 
     private let cellIdentifier = "ContactCell"
 
@@ -16,15 +18,33 @@ class NewChatViewController: UIViewController, UITableViewDataSource {
         dismissViewControllerAnimated(true, completion: nil)
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         return tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+        let name = chatNameField.text?.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        if name?.characters.count > 0 {
+
+        } else {
+            let alert = UIAlertController(title: "Enter a chat name first", message: nil, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(alert, animated: true, completion: nil)
+        }
     }
 }
