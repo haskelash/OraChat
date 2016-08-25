669fb0cc78b256520a61d8aef67723c7af3166c7
Haskel Ash <haskelash@gmail.com>
2016-08-25 12:16:56 -0700

    use two models, one chached full model and one search model. also add search cancel button

--- OraChat/ViewControllers/ChatsListViewController.swift
+++ OraChat/ViewControllers/ChatsListViewController.swift
@@ -12,13 +12,19 @@ class ChatsListViewController: UIViewController, UITableViewDataSource, UISearch
 
     @IBOutlet private var tableView: UITableView!
 
-    private var model = [Chat]()
+    private var searchModel: [Chat]?
+    private var fullModel = [Chat]()
+    private var activeModel: [Chat] {
+        get {
+            return searchModel ?? fullModel
+        }
+    }
     private var cellIdentifier = "ChatCell"
     private var plusButtonHeight: CGFloat = 72
 
     override func viewDidLoad() {
         ChatClient.getList(success: { chats in
-            self.model = chats
+            self.fullModel = chats
             self.tableView.reloadData()
         })
 
@@ -30,7 +36,7 @@ class ChatsListViewController: UIViewController, UITableViewDataSource, UISearch
             let insideChatVC = segue.destinationViewController as? InsideChatViewController,
             let selectedPath = tableView.indexPathForSelectedRow {
 
-            let chat = model[selectedPath.row]
+            let chat = activeModel[selectedPath.row]
             insideChatVC.chatID = chat.chatID
             insideChatVC.chatName = chat.name
             tableView.deselectRowAtIndexPath(selectedPath, animated: true)
@@ -38,16 +44,33 @@ class ChatsListViewController: UIViewController, UITableViewDataSource, UISearch
     }
 
     func addChat(chat: Chat) {
-        model.append(chat)
-        let path = NSIndexPath(forRow: self.model.count-1, inSection: 0)
+        fullModel.append(chat)
+        //TODO: only add to search model if chat matches search string by some criteria
+        searchModel?.append(chat)
+        let path = NSIndexPath(forRow: self.activeModel.count-1, inSection: 0)
         self.tableView.insertRowsAtIndexPaths([path], withRowAnimation: .None)
+    }
+
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        self.searchModel = nil
+        self.tableView.reloadData()
+
+        searchBar.text = nil
+        searchBar.setShowsCancelButton(false, animated: true)
+        searchBar.resignFirstResponder()
     }
 
     func searchBarSearchButtonClicked(searchBar: UISearchBar) {
         ChatClient.getList(search: searchBar.text, success: { chats in
-            self.model = chats
+            self.searchModel = chats
             self.tableView.reloadData()
         })
+
+        searchBar.setShowsCancelButton(false, animated: true)
         searchBar.resignFirstResponder()
     }
 
@@ -56,13 +79,13 @@ class ChatsListViewController: UIViewController, UITableViewDataSource, UISearch
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return model.count
+        return activeModel.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ChatCell
 
-        cell.inject(chat: model[indexPath.row])
+        cell.inject(chat: activeModel[indexPath.row])
 
         return cell
     }
